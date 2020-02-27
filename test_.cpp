73bc1171d8583da698a25237bac14bff7ee62485
Rednef92-JWH <rednef92@gmail.com>
2020-02-27 01:27:02 -0800

    C++_test
    comments.

---
+++ test_.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_xCode
+//
+//  Created by Root_ on 2/25/20.
+//  Copyright © 2020 Root_. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
